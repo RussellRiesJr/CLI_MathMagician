97d55c381654652209dbc7f472eae8c01a79fe8a
Russell Ries <riesru@gmail.com>
2016-07-26 11:47:52 -0500

    add basic testing and show menu option

---
+++ mathmagician.py
@@ -0,0 +1,9 @@
+
+
+class MathTest():
+
+  def show_options(self):
+    print("1. Generate Integers")
+    print("2. Generate Fibonacci")
+    print("3. Generate Primes")
+    print("4. Quit Program")
---
+++ test.py
@@ -0,0 +1,21 @@
+from mathmagician import *
+import unittest
+
+class TestMathMagician(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(self):
+      self.mathThing = MathTest()
+
+    def test_fibonacci(self):
+      fib = self.mathThing.generate_fibonacci(6)
+      self.assertEqual([0, 1, 1, 2, 3, 5], fib)
+
+    def test_integers(self):
+      integers = self.mathThing.generate_intergers(6)
+      self.assertEqual([0, 1, 2, 3, 4, 5], integers)
+
+    def test_primes(self):
+      primes = self.mathThing.generate_primes(6)
+      self.assertEqual([2, 3, 5, 7, 11, 13], primes)
+
